bdcac09e99d9ac4fc4875ff9194b9ce41e4d40e9
Libai <1915398623@qq.com>
2019-10-31 08:21:57 +0800

    update hwt

--- miniprogram/pages/publish/publish.js
+++ miniprogram/pages/publish/publish.js
@@ -1,10 +1,15 @@
 const db = wx.cloud.database(); // 初始化数据库
 const app = getApp(); // 获取全局数据
 
+// 引入SDK核心类
+var QQMapWX = require('../../static/qqmap-wx-jssdk.min.js');
+var qqmapsdk;
+
 Page({
   data:{
     tempImg: [],  // 临时图片存储
-    uploadImg: []  // 准备上传的图片集合
+    fileIDs: [],
+    address: '',  // 定位
   },
   // 上传图片到页面
   uploadImgHandle: function(e){
@@ -19,13 +24,11 @@ Page({
         // 如果有图片 则累加
         if (_this.data.tempImg) {
           let tempImgAdd = _this.data.tempImg.concat(tempFilePaths);
-          _this.data.uploadImg = tempImgAdd;  // 添加到准备上传图片的列表
           // 更新页面图片
           _this.setData({
             tempImg: tempImgAdd
           })
         } else {
-          _this.data.uploadImg = tempFilePaths;  // 添加到准备上传图片的列表
           // 图片显示到页面
           _this.setData({
             tempImg: tempFilePaths
@@ -33,7 +36,41 @@ Page({
         }
       }
     })
-    console.log(this.data.tempImg.length)
+  },
+  // 获取位置
+  location: function(e){
+    let _this = this;
+    // 获取经纬度
+    wx.getLocation({
+      success(res) {
+        const latitude = res.latitude;
+        const longitude = res.longitude;
+        _this.getLocal(latitude, longitude)
+      },
+      fail(err){
+        console.log('fail' + err)
+      }
+    })
+  },
+  // 腾讯地图api获取定位
+  getLocal: function(latitude, longitude){
+    let _this = this;
+    qqmapsdk.reverseGeocoder({
+      location: {
+        latitude: latitude,
+        longitude: longitude
+      },
+      success(res){
+        let province = res.result.ad_info.province;
+        let city = res.result.ad_info.city;
+        _this.setData({
+          address: province + city
+        })
+      },
+      fail(res){
+        console.log('fail' + res)
+      }
+    })
   },
   // 发布心情
   bindFormSubmit: function (e) {
@@ -45,32 +82,73 @@ Page({
       content: e.detail.value.textarea,    // 内容
       like: 0,      // 点赞
       comment: [],  // 评论
-      image: this.data.uploadImg
+      image: this.data.fileIDs,
+      address: this.data.address
     }
 
-    console.log(data)
+    if(this.data.tempImg){ // 发表图片
+      
+      //只能一张张上传 遍历临时的图片数组
+      const promiseArr = [];
 
-    // 获取用户信息
-    // wx.getSetting({
-    //   complete: function (res) {
-    //     if (res.authSetting['scope.userInfo']) { // 已授权
+      for (let i = 0; i < this.data.tempImg.length; i++) {
+        
+        let filePath = this.data.tempImg[i]
+        let suffix = /\.[^\.]+$/.exec(filePath)[0]; // 正则表达式，获取图片扩展名
+        
+        //在每次上传的时候，就往promiseArr里存一个promise，只有当所有的都返回结果时，才可以继续往下执行
+        promiseArr.push(new Promise((reslove, reject) => {
 
-    //       // 添加评论到数据库
-    //       db.collection('comment').add({
-    //         data: data
-    //       }).then(res => {
-    //         console.log('发表成功')
-    //       }).catch(err => {
-    //         console.log('发表失败')
-    //         console.log(err)
-    //       })
+          wx.cloud.uploadFile({
+            cloudPath: new Date().getTime() + suffix,
+            filePath: filePath, // 文件路径
+          }).then(res => {
+            
+            let newFileID = res.fileID; 
 
-    //     } else {
-    //       console.log('请登录')
-    //     }
-    //   }
-    // })
+            // 如果有图片 则累加
+            if (this.data.fileIDs) {
+              let fileIDsAdd = this.data.fileIDs.push(newFileID);
 
+              // console.log(fileIDsAdd)
+              // 更新页面图片
+              this.setData({
+                fileIDs: fileIDsAdd
+              })
+            } else {
+              // let fileIDsAdd = this.data.fileIDs.push(newFileID);
+              // // 图片显示到页面
+              // this.setData({
+              //   fileIDs: fileIDsAdd
+              // })
+              console.log(typeof this.data.fileIDs)
+            }
+            // console.log(newFileID)
+            console.log(data)
+
+            // console.log(this.data.fileIDs)
+          
+            reslove()
+          }).catch(error => {
+            console.log(error)
+            return;
+          })
+        }))
+        Promise.all(promiseArr).then(res => {
+          console.log('4444')
+        })
+      }
+    } else {
+      // 不发表图片 直接添加数据
+      db.collection('comment').add({
+        data: data
+      }).then(res => {
+        console.log('发表成功')
+      }).catch(err => {
+        console.log('发表失败')
+        console.log(err)
+      })
+    }
   },
   /**
    * 页面的初始数据
@@ -83,7 +161,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 实例化API核心类
+    qqmapsdk = new QQMapWX({
+      key: 'FVTBZ-O2BW5-7QUI2-QZ5PZ-INFPS-JTBOL'
+    });
   },
 
   /**
